590fd20f68fb5ec1a92728046b7ad60ac63fcd45
Jatin Mankar <jatinmankar@Jatins-MacBook-Pro.local>
2024-10-01 02:36:13 +0530

    Application Basics

---
+++ src/main/java/com/sinfolix/Sai_Samarth/SaiSamarthApplication.java
@@ -0,0 +1,13 @@
+package com.sinfolix.Sai_Samarth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaiSamarthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaiSamarthApplication.class, args);
+	}
+
+}
